5594c18bd76a1b3f5e6902264675f67e0fa03c6f
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-08-29 17:55:47 +0000

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module defines a class Square"""
+
+
+class Square:
+    """ a class for Square"""
+    def __init__(self, size):
+        """ it Initialize class """
+        self.__size = size
