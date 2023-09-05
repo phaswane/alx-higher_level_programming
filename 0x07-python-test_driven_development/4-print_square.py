ac9f728e7d4e403ac442f24b1744aa822afc9b66
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-09-05 17:55:56 +0000

    Function to print a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Function to print a square
+"""
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for index in range(size):
+        print("x" * size)
